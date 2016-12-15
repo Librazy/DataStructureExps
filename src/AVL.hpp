373ed875b86266f83e934b458d4a571694273308
Librazy <librazy@foxmail.com>
2016-12-15 23:09:00 +0800

    -

--- src/AVL.hpp
+++ src/AVL.hpp
@@ -8,47 +8,96 @@
 #include <memory>
 #include "BinaryTree.hpp"
 
+/**
+ * \brief 节点数据类型
+ * \tparam T 存储类型
+ */
 template<typename T>
 struct avl_data
 {
+	/**
+	 * \brief 默认构造
+	 */
 	avl_data()
 		: height(1), size(1), val(T()) {}
 
+	/**
+	 * \brief 使用给定数据构造
+	 * \tparam K 传入数据类型
+	 * \param d 传入数据
+	 */
 	template<typename K>
 	explicit avl_data(K&& d)
 		: height(1), size(1), val(std::forward<K>(d)) {}
+
+	/**
+	 * \brief 子树高
+	 */
 	size_t height;
 
+	/**
+	 * \brief 子树大小
+	 */
 	size_t size;
 
+	/**
+	 * \brief 数据
+	 */
 	T val;
-
-	T* operator->() {
-		return &val;
-	}
 };
 
-template<template<class...> class P, typename T>
+/**
+ * \brief 节点构造器
+ * \tparam P 包装类型
+ */
+template<template<class...> class P>
 struct ptr_maker
 {
+	/**
+	 * \brief 构造给定类型节点
+	 * \tparam N 节点类型
+	 * \tparam K 传入数据类型
+	 * \param arg 传入数据
+	 * \return 节点
+	 */
 	template<typename N, typename K>
 	P<N> make(K&& arg){
 		return P<N>(new N(std::forward<K>(arg)));
 	};
 };
 
-template<typename T>
-struct ptr_maker<std::shared_ptr, T>
+/**
+ * \brief std::shared_ptr节点构造器
+ */
+template<>
+struct ptr_maker<std::shared_ptr>
 {
+	/**
+	 * \brief 构造给定std::shared_ptr类型节点
+	 * \tparam N 节点类型
+	 * \tparam K 传入数据类型
+	 * \param arg 传入数据
+	 * \return 节点
+	 */
 	template<typename N, typename K>
 	std::shared_ptr<N> make(K&& arg) {
 		return std::make_shared<N>(std::forward<K>(arg));
 	};
 };
 
-template<typename T>
-struct ptr_maker<std::unique_ptr, T>
+/**
+ * \brief std::unique_ptr节点构造器
+ */
+template<>
+struct ptr_maker<std::unique_ptr>
 {
+	/**
+	 * \brief 构造给定std::unique_ptr类型节点
+	 * \tparam N 节点类型
+	 * \tparam K 传入数据类型
+	 * \param arg 传入数据
+	 * \return 节点
+	 */
 	template<typename N, typename K>
 	std::unique_ptr<N> make(K&& arg) {
 		return std::make_unique<N>(std::forward<K>(arg));
@@ -56,7 +105,14 @@ struct ptr_maker<std::unique_ptr, T>
 };
 
 
-template<typename T, typename Compare = std::less<>,template<class...> class P = std::unique_ptr, typename Make = ptr_maker<P, T>>
+/**
+ * \brief AVL树
+ * \tparam T 存储类型
+ * \tparam Compare 比较器类型
+ * \tparam P 包装类型
+ * \tparam Make 节点构造器类型
+ */
+template<typename T, typename Compare = std::less<>,template<class...> class P = std::unique_ptr, typename Make = ptr_maker<P>>
 class avl_tree{
 	template<typename V>
 	class avl_it;
@@ -79,129 +135,265 @@ public:
 private:
 
 
+	/**
+	 * \brief 节点类型
+	 */
 	struct avl_node : bt_t
 	{
+		/**
+		 * \brief 虚析构
+		 */
 		virtual ~avl_node() {}
 
+		/**
+		 * \brief 默认构造
+		 */
 		avl_node() : bt_t(avl_data<T>())
 		{ };
 
+		/**
+		 * \brief 使用给定数据构造
+		 * \tparam K 传入数据类型
+		 * \param val 传入数据
+		 */
 		template<typename K>
 		explicit avl_node(K&& val) : bt_t(avl_data<T>(std::forward<K>(val)))
 		{ };
 	};
 
+	/**
+	 * \brief 迭代器类型
+	 * \tparam V 解引用类型
+	 */
 	template<typename V>
 	class avl_it
 	{
+		/**
+		 * \brief 当前数据索引
+		 */
 		size_type current;
+
+
+		/**
+		 * \brief 所属容器指针
+		 */
 		avl_tree* pt;
 
+		/**
+		 * \brief 私有构造
+		 * \param current 数据索引
+		 * \param pt 所属容器指针
+		 */
 		avl_it(size_type current, avl_tree* pt) :current(current), pt(pt)
 		{ }
 
 	public:
 		friend class avl_tree;
 
+		/**
+		 * \brief 默认构造
+		 */
 		avl_it() :current(0), pt(nullptr)
 		{ }
 
+		/**
+		 * \brief 偏移量+迭代器
+		 * \param i 偏移量
+		 * \param a 迭代器
+		 * \return 目标迭代器
+		 */
 		friend iterator operator+(difference_type i, iterator const& a) {
 			auto res = avl_it(a);
 			res.current += i;
 			return res;
 		}
 
+		/**
+		 * \brief 迭代器前自增
+		 * \return 自增后迭代器
+		 */
 		avl_it operator++() {
 			++current;
 			return *this;
 		}
 
+		/**
+		 * \brief 迭代器前自增
+		 * \return 自增前迭代器
+		 */
 		avl_it operator++(int) {
 			auto res = avl_it(*this);
 			++current;
 			return res;
 		}
 
+		/**
+		 * \brief 迭代器前自增
+		 * \return 自增后迭代器
+		 */
 		avl_it operator--() {
 			--current;
 			return *this;
 		}
 
+		/**
+		 * \brief 迭代器后自减
+		 * \return 自减前迭代器
+		 */
 		avl_it operator--(int) {
 			auto res = avl_it(*this);
 			--current;
 			return res;
 		}
 
+		/**
+		 * \brief 迭代器+=偏移量
+		 * \param i 偏移量
+		 * \return 目标迭代器
+		 */
 		avl_it& operator+=(difference_type i) {
 			current += i;
 			return this;
 		}
 
+		/**
+		 * \brief 迭代器-=偏移量
+		 * \param i 偏移量
+		 * \return 目标迭代器
+		 */
 		avl_it& operator-=(difference_type i) {
 			current -= i;
 			return this;
 		}
 
+		/**
+		 * \brief 迭代器-偏移量
+		 * \param i 偏移量
+		 * \return 目标迭代器
+		 */
 		avl_it operator-(difference_type i) {
 			auto res = avl_it(*this);
 			res.current -= i;
 			return res;
 		}
 
+		/**
+		 * \brief 迭代器-目标迭代器
+		 * \param i 目标迭代器
+		 * \return 偏移量
+		 */
 		difference_type operator-(avl_it const& i) {
 			return current - i.current;
 		}
 
+		/**
+		 * \brief 迭代器+偏移量
+		 * \param i 偏移量
+		 * \return 目标迭代器
+		 */
 		avl_it operator+(difference_type i) {
 			return i + *this;
 		}
 
+		/**
+		 * \brief 迭代器相等
+		 * \param a 目标迭代器
+		 * \return ==
+		 */
 		bool operator==(avl_it const& a) const {
-			return a.current == current && a.pt == pt;
+			return a.current == current;
 		}
 
+		/**
+		 * \brief 迭代器小于
+		 * \param a 目标迭代器
+		 * \return <
+		 */
 		bool operator<(avl_it const& a) const {
 			return a.current < current;
 		}
 
+		/**
+		 * \brief 迭代器大于
+		 * \param a 目标迭代器
+		 * \return >
+		 */
 		bool operator>(avl_it const& a) const {
 			return a.current > current;
 		}
-
+		
+		/**
+		 * \brief 迭代器小于等于
+		 * \param a 目标迭代器
+		 * \return <=
+		 */
 		bool operator<=(avl_it const& a) const {
 			return a.current <= current;
 		}
 
+		/**
+		 * \brief 迭代器大于等于
+		 * \param a 目标迭代器
+		 * \return >=
+		 */
 		bool operator>=(avl_it const& a) const {
 			return a.current >= current;
 		}
 
+		/**
+		 * \brief 迭代器不等于
+		 * \param a 目标迭代器
+		 * \return !=
+		 */
 		bool operator!=(avl_it const& a) const {
-			return !(a == *this);
+			return a.current != current;
 		}
 
+		/**
+		 * \brief **O(log n) **解引用
+		 * \return 指向的元素的引用
+		 */
 		V& operator*() {
 			return (*pt).nth(current);
 		}
 
+		/**
+		 * \brief **O(log n) **解指针
+		 * \return 指向的元素的指针
+		 */
 		V* operator->() {
 			return &((*pt).nth(current));
 		}
-
+		/**
+		 * \brief **O(log n) **解引用偏移量
+		 * \return 指向的给定偏移量元素的引用
+		 */
 		V& operator[](size_type s)
 		{
 			return &((*pt).nth(current + s));
 		}
 	};
 
+	/**
+	 * \brief 比较器
+	 */
 	static key_compare comp;
 
+	/**
+	 * \brief 节点构造器
+	 */
 	static node_make maker;
 
+	/**
+	 * \brief 根节点
+	 */
 	node_t root;
 
+	/**
+	 * \brief 获取子树高
+	 * \param cur 给定节点
+	 * \return 子树高
+	 */
 	static size_t get_height(node_t const& cur)
 	{
 		if(cur) {
@@ -210,6 +402,11 @@ private:
 		return 0;
 	}
 
+	/**
+	 * \brief 获取子树大小
+	 * \param cur 给定节点
+	 * \return 子树大小
+	 */
 	static size_t get_size(node_t const& cur)
 	{
 		if (cur) {
@@ -218,6 +415,10 @@ private:
 		return 0;
 	}
 
+	/**
+	 * \brief 判断子树是否需要旋转
+	 * \param cur 子树
+	 */
 	static void check_rotate(node_t& cur)
 	{
 		difference_type det = get_height(cur->left) - get_height(cur->right);
@@ -243,6 +444,10 @@ private:
 		}
 	}
 
+	/**
+	 * \brief LL旋转
+	 * \param cur 子树
+	 */
 	static void rotate_ll(node_t& cur)
 	{
 		//LL
@@ -276,6 +481,10 @@ private:
 		maintain_node(cur->right);
 	}
 
+	/**
+	 * \brief LR旋转
+	 * \param cur 子树
+	 */
 	static void rotate_lr(node_t& cur)
 	{
 		//LR
@@ -319,6 +528,10 @@ private:
 		maintain_node(cur->left);
 	}
 
+	/**
+	 * \brief RR旋转
+	 * \param cur 子树
+	 */
 	static void rotate_rr(node_t& cur)
 	{
 		//RR
@@ -352,6 +565,10 @@ private:
 		maintain_node(cur->left);
 	}
 
+	/**
+	 * \brief RL旋转
+	 * \param cur 子树
+	 */
 	static void rotate_rl(node_t& cur)
 	{
 		//RL
@@ -395,11 +612,19 @@ private:
 		maintain_node(cur->left);
 	}
 
+	/**
+	 * \brief 维护节点数据
+	 * \param cur 节点
+	 */
 	static void maintain_node(node_t& cur)
 	{
 		maintain_node(cur.get());
 	}
 
+	/**
+	 * \brief 维护节点数据
+	 * \param cur 节点裸指针
+	 */
 	static void maintain_node(bt_t* cur)
 	{
 		auto ls = cur->left ? cur->left->data.size: 0;
@@ -410,6 +635,14 @@ private:
 		cur->data.size = ls + rs + 1;
 	}
 
+	/**
+	 * \brief 搜索实现
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \param cur 当前节点
+	 * \param s 输出当前节点rank
+	 * \return 目标存储引用
+	 */
 	template <typename K>
 	static T& search_impl(K&& t, node_t const& cur, size_type& s)
 	{
@@ -431,6 +664,12 @@ private:
 		return search_impl(t, cur->right, s);
 	}
 
+	/**
+	 * \brief 查询指定rank元素实现
+	 * \param s 查询rank
+	 * \param cur 当前节点
+	 * \return 目标存储引用
+	 */
 	static T& nth_impl(size_type s, node_t const& cur)
 	{
 		if (s > get_size(cur)) {
@@ -446,6 +685,13 @@ private:
 		return nth_impl(det, cur->right);
 	}
 
+	/**
+	 * \brief 插入实现
+	 * \tparam K 传入存储类型
+	 * \param t 待插入存储
+	 * \param cur 当前节点
+	 * \return 是否插入
+	 */
 	template <typename K>
 	static bool insert_impl(K&& t, node_t& cur) noexcept
 	{
@@ -479,6 +725,11 @@ private:
 		return true;
 	}
 
+	/**
+	 * \brief 弹出目标节点中序前驱（左子树存在）
+	 * \param cur 目标节点
+	 * \return 弹出的节点
+	 */
 	static node_t extract_max(node_t& cur)
 	{
 		auto res = node_t();
@@ -504,6 +755,11 @@ private:
 		return res;
 	}
 
+	/**
+	 * \brief 弹出目标节点中序后继（右子树存在）
+	 * \param cur 目标节点
+	 * \return 弹出的节点
+	 */
 	static node_t extract_min(node_t& cur)
 	{
 		auto res = node_t();
@@ -528,7 +784,15 @@ private:
 		return res;
 	}
 
-	static bool remove_impl(T&& t, node_t& cur)
+	/**
+	 * \brief 删除实现
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \param cur 当前节点
+	 * \return 是否删除
+	 */
+	template <typename K>
+	static bool remove_impl(K&& t, node_t& cur)
 	{
 		if (!cur) {
 			return false;
@@ -566,17 +830,45 @@ private:
 
 public:
 
-	avl_tree()
+	/**
+	 * \brief 默认构造
+	 */
+	avl_tree() noexcept
 	{ }
 
-	explicit avl_tree(T&& t) : root(maker.template make<avl_node>(std::forward<T>(t)))
+	/**
+	 * \brief 使用给定存储初始化根节点
+	 * \param t 根节点存储
+	 */
+	explicit avl_tree(T&& t) noexcept : root(maker.template make<avl_node>(std::forward<T>(t)))
 	{ }
 
+	/**
+	 * \brief 移动构造
+	 * \param a 目标AVL
+	 */
+	avl_tree(avl_tree&& a) noexcept
+	{
+		std::swap(a.root, root);
+	}
+
+	/**
+	 * \brief 插入
+	 * \tparam K 传入存储类型
+	 * \param t 待插入存储
+	 * \return 是否插入
+	 */
 	template <typename K>
 	bool insert(K&& t) noexcept {
 		return insert_impl(std::forward<K>(t), root);
 	}
 
+	/**
+	 * \brief 搜索
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \return 目标存储只读引用
+	 */
 	template <typename K>
 	const_reference search(K&& t) const
 	{
@@ -584,14 +876,27 @@ public:
 		return search_impl(std::forward<K>(t), root, ig);
 	}
 
+	/**
+	 * \brief 根据key搜索pair
+	 * \tparam K 传入查询类型
+	 * \tparam ST = T
+	 * \param t 查询数据
+	 * \return 目标pair的第二个元素的引用
+	 */
 	template <typename K, typename ST = T>
 	typename ST::second_type& search_pair_key(K&& t) const
 	{
 		size_type ig;
 		auto& ret = search_impl(std::forward<K>(t), root, ig);
-		return ret.second;
+		return std::get<1>(ret);
 	}
 
+	/**
+	 * \brief 查询rank
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \return 目标存储rank
+	 */
 	template <typename K>
 	size_type rank(K&& t) const
 	{
@@ -600,6 +905,12 @@ public:
 		return res;
 	}
 
+	/**
+	 * \brief 查询迭代器
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \return 目标迭代器
+	 */
 	template <typename K>
 	iterator find(K&& t) const noexcept
 	{
@@ -612,67 +923,124 @@ public:
 		}
 	}
 
+	/**
+	 * \brief 删除
+	 * \tparam K 传入查询类型
+	 * \param t 查询数据
+	 * \return 是否删除
+	 */
 	template <typename K>
 	bool remove(K&& t) noexcept
 	{
 		return remove_impl(std::forward<K>(t), root);
 	}
 
+	/**
+	 * \brief 指定rank元素
+	 * \param s rank
+	 * \return 目标存储只读引用
+	 */
 	const_reference nth(size_type s) const
 	{
 		return nth_impl(s + 1, root);
 	}
 
+	/**
+	 * \brief AVL大小
+	 * \return 当前AVL大小
+	 */
 	size_type size() const noexcept
 	{
 		return get_size(root);
 	}
 
+	/**
+	 * \brief AVL树高
+	 * \return 当前AVL树高
+	 */
 	size_type height() const noexcept
 	{
 		return get_height(root);
 	}
 
+	/**
+	 * \brief 是否为空
+	 * \return 是否为空
+	 */
 	bool empty() const noexcept
 	{
 		return root;
 	}
 
+	/**
+	 * \brief 交换AVL
+	 * \param another 目标AVL
+	 */
 	void swap(avl_tree another) noexcept
 	{
 		std::swap(root, another.root);
 	}
 
+	/**
+	 * \brief 头迭代器
+	 * \return 头迭代器
+	 */
 	iterator begin() & noexcept
 	{
 		return iterator(0, this);
 	}
 
+	/**
+	 * \brief 头迭代器
+	 * \return 头迭代器
+	 */
 	iterator end() & noexcept
 	{
 		return iterator(size(), this);
 	}
-
+	
+	/**
+	 * \brief 只读头迭代器
+	 * \return 只读头迭代器
+	 */
 	iterator begin() const& noexcept
 	{
 		return cbegin();
 	}
-
+	
+	/**
+	 * \brief 只读尾迭代器
+	 * \return 只读尾迭代器
+	 */
 	iterator end() const& noexcept
 	{
 		return cend();
 	}
-
+	
+	/**
+	 * \brief 只读头迭代器
+	 * \return 只读头迭代器
+	 */
 	const_iterator cbegin() const& noexcept
 	{
 		return const_iterator(0, this);
 	}
-
+	
+	/**
+	 * \brief 只读尾迭代器
+	 * \return 只读尾迭代器
+	 */
 	const_iterator cend() const& noexcept
 	{
 		return const_iterator(size(), this);
 	}
 
+	/**
+	 * \brief AVL递归遍历
+	 * \tparam O 遍历方式
+	 * \tparam F 操作函数类型
+	 * \param f 操作函数
+	 */
 	template<Order O, typename F>
 	void traversal_recursive(F&& f) const
 	{
